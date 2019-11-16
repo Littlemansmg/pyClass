c5a9cbedbfb4bbb35ad148cfe964110296a01f15
Scott Goes <littlemansmg@gmail.com>
2019-11-15 22:43:56 -0600

    8-1 complete

--- Week 3 Assn/Project 8/8-1.py
+++ Week 3 Assn/Project 8/8-1.py
@@ -1,8 +1,43 @@
 # created by Scott "LittlemanSMG" Goes on DD/MM/YYYY
 
 
+def get_info():
+    cost = float
+    tip = int
+    while True:
+        try:
+            cost = float(input("Cost of meal: "))
+            if cost < 0:
+                print("Must be greater than 0. Please try again.")
+                continue
+        except ValueError:
+            print("Must be a valid decimal number. Please try again.")
+            continue
+        while True:
+            try:
+                tip = int(input('Tip percent: '))
+            except ValueError:
+                print('Must be a valid integer. Please Try again.')
+                continue
+            break
+        break
+
+    tip_calc(cost, tip)
+
+
+def tip_calc(cost, tip):
+    tip_total = round(cost * (tip/100), 2)
+    total = round(tip_total + cost, 2)
+
+    print("\nCost of meal: \t{}\n"
+          "Tip percent:\t{}%\n"
+          "Tip amount:\t\t{}\n"
+          "Total amount:\t{}".format(cost, tip, tip_total, total))
+
+
 def main():
-    print("This is the start")
+    print("Tip Calculator")
+    get_info()
 
 
 if __name__ == "__main__":
