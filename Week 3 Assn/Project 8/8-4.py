51898e29727634d6725b5910b6f91f03875f10cc
Scott Goes <littlemansmg@gmail.com>
2019-11-15 23:25:30 -0600

    8-4 complete

--- Week 3 Assn/Project 8/8-4.py
+++ Week 3 Assn/Project 8/8-4.py
@@ -1,9 +1,83 @@
 # created by Scott "LittlemanSMG" Goes on DD/MM/YYYY
 
 
+import csv
+import time
+import os
+
+
+def cmd_monthly():
+    for line in sales:
+        print('{} - {}'.format(line[0], line[1]))
+    time.sleep(2)
+
+
+def cmd_yearly():
+    yearly_total = 0.0
+    for line in sales:
+        try:
+            float(line[1])
+            yearly_total += float(line[1])
+        except ValueError:
+            print("Using sales amount of 0 for {}".format(line[0]))
+
+
+    average = round(yearly_total / 12, 2)
+
+    print('Yearly total:     {}\n'
+          'Monthly average:  {}'.format(yearly_total, average))
+
+
+def cmd_edit():
+    new_month = input("Three-letter Month: ")
+    new_sales = input("Sales amount: ")
+    for i in range(len(sales) + 1):
+        if new_month == sales[i][0]:
+            sales[i][1] = new_sales
+            break
+    with open('./monthly_sales.csv', 'w') as csvfile:
+        write = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+        for row in sales:
+            write.writerow(row)
+
+
+def cmd_exit():
+    print('Bye!')
+    exit()
+
+
+def command_menu():
+    print("COMMAND MENU\n"
+          "monthly - View monthly sales\n"
+          "yearly  - View yearly summary\n"
+          "edit    - Edit sales for a month\n"
+          "exit    - Exit the program")
+
+
 def main():
-    print("This is the start")
+    print('Monthly Sales Program')
+    while True:
+        command_menu()
+        command = input("Command: ")
+        if command.lower() == 'monthly':
+            cmd_monthly()
+        elif command.lower() == 'yearly':
+            cmd_yearly()
+        elif command.lower() == 'edit':
+            cmd_edit()
+        elif command.lower() == 'exit':
+            print("Bye!")
+            exit()
 
 
 if __name__ == "__main__":
+    sales = []
+    if not os.path.exists('./monthly_sales.csv'):
+        print("Can't find file. Bye!")
+        exit()
+    with open('monthly_sales.csv', 'r') as csfile:
+        reader = csv.reader(csfile.readlines())
+        for line in reader:
+            sales.append(line)
+
     main()
