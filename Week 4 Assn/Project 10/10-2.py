548d8609ed7c89afd491ae4e9ebfb6042a79e4f6
Scott Goes <littlemansmg@gmail.com>
2019-11-19 13:52:38 -0600

    10-2 done

--- Week 4 Assn/Project 10/10-2.py
+++ Week 4 Assn/Project 10/10-2.py
@@ -1,9 +1,31 @@
-# created by Scott "LittlemanSMG" Goes on DD/MM/YYYY
+# created by Scott "LittlemanSMG" Goes on 11/19/2019
+
+
+import re
+
+
+def mass_template():
+    for i in contacts:
+        print(template.format(i[2].lower(), i[0].capitalize()))
 
 
 def main():
-    print("This is the start")
+    mass_template()
 
 
 if __name__ == "__main__":
+    contacts = []
+    template = str
+    try:
+        with open('./email.txt', 'r') as file:
+            for i in file:
+                contacts.append(re.split('; |,|\n', i))
+
+        with open('./template_email.txt') as file:
+            template = str(file.read())
+
+    except FileNotFoundError:
+        print("Can't find files.\nBye!")
+        exit()
+
     main()
