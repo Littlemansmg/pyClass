16175ad4ead3f5b6f3b1303bb2f1618dee74661b
Scott Goes <littlemansmg@gmail.com>
2019-11-22 23:12:03 -0600

    12-3 complete

--- Week 4 Assn/Project 12/12-3.py
+++ Week 4 Assn/Project 12/12-3.py
@@ -2,10 +2,32 @@
 
 
 def main():
-    print("This is the start")
+    print(f"Country\t\tWins\tYears\n"
+          f"=======\t\t====\t=====")
+    for key in sorted(teams.keys()):
+        year = teams[key]['year']
+        if key == 'Argentina':
+            print(f"{key}\t{len(teams[key]['year'])}\t\t", end='')
+            print(*year, sep=', ')
+        else:
+            print(f"{key}\t\t{len(teams[key]['year'])}\t\t", end='')
+            print(*year, sep=', ')
 
 
 if __name__ == "__main__":
-    # fifa_www = {}
-    # with
+    teams = {}
+    with open('./world_cup_champions.txt', 'r') as file:
+        for line in file.readlines():
+            line = line.split(',')
+
+            if line[1] not in teams.keys():
+                teams[line[1]] = {'year': [line[0]]}
+            else:
+                teams[line[1]]['year'].append(line[0])
+
+            if 'coach' not in teams.keys():
+                teams[line[1]].update({'coach': line[2]})
+
+            if 'capitan' not in teams.keys():
+                teams[line[1]].update({'capitan': line[3]})
     main()
