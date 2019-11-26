5a113d3479f1938858c193fa1508ebe115e2dc92
Scott Goes <littlemansmg@gmail.com>
2019-11-26 17:21:52 -0600

    14-1 complete

--- Week 5 Assn/Project 14/14-1.py
+++ Week 5 Assn/Project 14/14-1.py
@@ -1,8 +1,57 @@
 # created by Scott "LittlemanSMG" Goes on DD/MM/YYYY
 
 
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return (self.height + self.width) * 2
+
+    def show_rect(self):
+        temp = len("* " * self.width)
+        print("* " * self.width)
+        for i in range(self.height - 2):
+            print("*" + " " * (temp - 3) + "*")
+        print("* " * self.width)
+
+
 def main():
-    print("This is the start")
+    print("Rectangle Calculator")
+    while True:
+        height, width = 0, 0
+        while True:
+            try:
+                height = int(input('Height:\t   '))
+                if height <= 0:
+                    raise ValueError
+            except ValueError:
+                print("You must put in an integer.")
+                continue
+            break
+
+        while True:
+            try:
+                width = int(input('Width: \t   '))
+                if width <= 0:
+                    raise ValueError
+            except ValueError:
+                print("You must put in an integer.")
+                continue
+            break
+
+        new_shape = Rectangle(height, width)
+        print(f"Perimeter: {new_shape.perimeter()}")
+        print(f"Area:\t   {new_shape.area()}")
+        new_shape.show_rect()
+        end = input("\nContinue? (y/n): ")
+        print()
+        if end.lower() == 'n':
+            exit()
 
 
 if __name__ == "__main__":
