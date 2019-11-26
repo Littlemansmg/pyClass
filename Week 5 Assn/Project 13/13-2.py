926ad508efb66dee6d830241ab49d198db52b763
Scott Goes <littlemansmg@gmail.com>
2019-11-26 14:55:16 -0600

    13-2 complete

--- Week 5 Assn/Project 13/13-2.py
+++ Week 5 Assn/Project 13/13-2.py
@@ -1,8 +1,29 @@
 # created by Scott "LittlemanSMG" Goes on DD/MM/YYYY
 
 
+def display_branch(branch):
+    if branch == 0:
+        pass
+    else:
+        display_branch(branch - 1)
+        print("*****" * branch)
+        display_branch(branch - 1)
+
+
 def main():
-    print("This is the start")
+    print("Tree Pattern")
+    branches = 1
+    while True:
+        try:
+            branches = int(input("Enter number of branches: "))
+            if branches < 0:
+                raise ValueError
+        except ValueError:
+            print("You need to enter a number.")
+            continue
+        break
+    display_branch(branches)
+
 
 
 if __name__ == "__main__":
