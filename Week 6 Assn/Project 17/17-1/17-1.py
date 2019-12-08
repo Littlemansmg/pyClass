01b7ac1805abd7581ba965b4828bf4ba9f35ceb9
Scott Goes <littlemansmg@gmail.com>
2019-12-07 23:07:31 -0600

    17-1 complete

--- Week 6 Assn/Project 17/17-1/17-1.py
+++ Week 6 Assn/Project 17/17-1/17-1.py
@@ -2,33 +2,32 @@ import sqlite3
 import csv
 
 
-
 def main():
-    with sqlite3.connect('customers') as db:
-        db.execute("SELECT * FROM Customer")
-    # i = 100
-    # mydb.execute("SELECT * FROM Customer")
-    # with open('customers.csv', 'r') as file:
-    #     reader = csv.DictReader(file)
-    #     for row in reader:
-    #         i += 1
-    #         keys = list(row.keys())
-    #
-    #         test = f"INSERT INTO Customer Values(?,?,?,?,?,?,?,?),({i}, {row[keys[0]]}, {row[keys[1]]}," \
-    #                f"{row[keys[2]]}, {row[keys[3]]}, {row[keys[4]]}, {row[keys[5]]}, {row[keys[6]]})"
-    #
-    #         # sqlstring = f'INSERT INTO Customer VALUES (' \
-    #         #             f'{i},' \
-    #         #             f'{row[keys[0]]},' \
-    #         #             f'{row[keys[1]]},' \
-    #         #             f'{row[keys[2]]},' \
-    #         #             f'{row[keys[3]]},' \
-    #         #             f'{row[keys[4]]},' \
-    #         #             f'{row[keys[5]]},' \
-    #         #             f'{row[keys[6]]};)'
-    #         mydb.execute("INSERT INTO Customer Values(?,?,?,?,?,?,?,?)",
-    #                      (i, row[keys[0]], row[keys[1]], row[keys[2]], row[keys[3]],
-    #                       row[keys[4]], row[keys[5]], row[keys[6]]))
+    connect = sqlite3.connect("customers.sqlite")
+    cursor = connect.cursor()
+
+    cursor.execute("DELETE FROM Customer")
+
+    i = 100
+    with open('customers.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            i += 1
+            keys = list(row.keys())
+
+            cursor.execute("INSERT INTO Customer Values(?,?,?,?,?,?,?,?)",
+                           (i, row[keys[0]], row[keys[1]], row[keys[2]], row[keys[3]],
+                            row[keys[4]], row[keys[5]], row[keys[6]]))
+            cursor.execute("Select * from customer")
+
+        connect.commit()
+
+    print("Customer Data Importer.\n\n"
+          "CSV file:\tcustomers.csv\n"
+          "DB file:\tcustomers.sqlite\n"
+          "Table name: Customer\n\n"
+          "All old rows deleted from Customer table.\n"
+          "{} rows(s) inserted into Customer table.".format(i-100))
 
 
 if __name__ == "__main__":
